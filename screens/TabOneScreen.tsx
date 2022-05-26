9efc211367d4dcb4a82ce98b7d5767c63894b426
Marc Qualie <marc@marcqualie.com>
2022-05-26 02:04:51 +0100

    Add placeholders with NNativeBase

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,18 @@
+import { FontAwesome } from '@expo/vector-icons';
+import { Box, Button } from 'native-base';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    <Box padding={10}>
+
+      <Button leftIcon={<FontAwesome name='calendar' />}>
+        Book an appointment
+      </Button>
+    </Box>
+  )
 }
 
 const styles = StyleSheet.create({
