0d3dbcf21c6f1a65ce382646614b54ecdf93d470
Alex <alexhouse@users.noreply.github.com>
2022-05-30 11:55:57 +0100

    Simple API service (#1)
    * Add a simple API service that can be started via `yarn api`
    
    * Simple start to a Readme
    
    * Dates in the past will fail API
    
    Co-authored-by: Marc Qualie <marc@marcqualie.com>

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const {DateTime} = require('luxon')
+
+app.listen(3005)
+app.use(express.json())
+
+app.get('/api/appointments', (req, res) => {
+  const now = DateTime.now()
+  const tomorrow = now.plus({days: 1}).toUTC().set({hour: 10, minute: 0, second: 0, millisecond: 0})
+
+  res.json({
+    appointments: (new Array(12)).fill(null).map((_, idx) => ({
+      date: tomorrow.plus({minutes: idx * 15}).toISO(),
+      vet: 'Dr. Smith',
+    }))
+  })
+})
+
+app.post('/api/appointments', (req, res) => {
+  const date = DateTime.fromISO(req.body.date) // TODO: Verification
+
+  if (date < DateTime.now()) {
+    res.status(400).json({
+      error: 'Appointment cannot be in the past',
+    })
+  } else {
+    res.json({
+      date: date,
+      message: 'Appointment booked',
+    })
+  }
+})
