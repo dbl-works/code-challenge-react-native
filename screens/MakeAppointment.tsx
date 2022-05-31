154636a7aec13ea4ecb5cc0ebae7c13efb57be1a
Alex House <house.alexander@gmail.com>
2022-05-31 17:49:51 +0100

    Remove unused import

--- screens/MakeAppointment.tsx
+++ screens/MakeAppointment.tsx
@@ -1,13 +1,12 @@
 import { FontAwesome } from "@expo/vector-icons";
-import { Box, Button, Icon } from 'native-base';
+import { Box, Button } from 'native-base';
 
 import { RootTabScreenProps } from '../types';
 
 export default function MakeAppointment({navigation}: RootTabScreenProps<'MakeAppointment'>) {
   return (
-    <Box padding={10}>
-
-      <Button leftIcon={<FontAwesome name="calendar"/>}>
+    <Box p={10}>
+      <Button startIcon={<FontAwesome name="calendar" />}>
         Find an appointment
       </Button>
     </Box>
